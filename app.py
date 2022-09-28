c012af2b440f03b428af94e23b33ef9dfc8d7831
HanslettTheDev <HanslettTheDev@gmail.com>
2022-09-28 18:23:29 +0100

    refactor: removed the interface_windows directory. not needed after the initial bug was solved

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from typepen.interface_windows.create_window import CreateWindow
 
 if __name__ == '__main__':
     api = API()
-    app = CreateWindow(server=server, debug=True, width=642, height=300, window_frame=True, js_api=api)
-    app.initWindow()
-    # server.run(port="3000", debug=True)
-    # window = webview.create_window('TypePen', server, width=642, height=300, js_api=api, frameless=True)
-    # webview.start(debug=True)
+    # app = CreateWindow(server=server, debug=True, width=642, height=300, window_frame=True, js_api=api)
+    # app.initWindow()
+    server.run(port="3000", debug=True)
+    window = webview.create_window('TypePen', server, width=642, height=300, js_api=api, frameless=True)
+    webview.start(debug=True)
